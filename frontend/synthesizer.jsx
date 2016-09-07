21d48cb7602bf0729eabb5e48fced0b0f2602d17
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-09-06 21:05:59 -0700

    finished synthesizer

--- frontend/synthesizer.jsx
+++ frontend/synthesizer.jsx
@@ -5,8 +5,20 @@ import { applyMiddleware } from 'redux';
 import configureStore from './store/store';
 import Root from './components/root';
 
+function addLoggingToDispatch(store) {
+  console.log("MADE IT");
+  let storeDispatch = store.dispatch
+  return (action) => {
+    console.log(store.getState());
+    console.log(action);
+    storeDispatch(action);
+    console.log(store.getState());
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
+  addLoggingToDispatch(store);
   const rootEl = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, rootEl);
 });
